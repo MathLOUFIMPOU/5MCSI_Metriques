f63821b171c1e755b707ad206a54d38354d94f05
MathLOUFIMPOU <165168000+MathLOUFIMPOU@users.noreply.github.com>
2024-06-07 11:35:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,8 +32,8 @@ def mongraphique():
     return render_template("graphique.html")
   
   @app.route("/rapport/")
-def monhistogramme():
-    return render_template("histogramme.html")
+def monHistogramme():
+    return render_template("Histogramme.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
